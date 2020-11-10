421d8e9904505e2b94f48f31864bd4ff849e3fbb
YunDaYoung <ydiosa98@gmail.com>
2020-11-11 08:06:43 +0900

    QR코드 스캔 시 행사 상세보기 이동

--- app/screens/ScanScreen.js
+++ app/screens/ScanScreen.js
@@ -1,23 +1,37 @@
 import React from "react";
 import {
     StyleSheet,
-    ScrollView,
     View,
     Text,
     Dimensions,
-    Image,
-    StatusBar
+    StatusBar,
+    ToastAndroid,
 } from 'react-native';
 import { Container, Header, Left, Body, Button, Right, Title } from 'native-base';
-import Feather from 'react-native-vector-icons/Feather';
 import AntDesign from 'react-native-vector-icons/AntDesign';
+import QRCodeScanner from 'react-native-qrcode-scanner';
+
 
 const SCREEN_HEIGHT = Dimensions.get("window").height;
 
-const ScanScreen = ({ navigation, route }) => {
-    return (
-        <View>
-            <Header style={{ backgroundColor: '#311957' }}>
+class ScanScreen extends React.Component {
+    state = {
+        result: null
+    }
+
+    onSuccess = async (elem) => {
+        this.setState({
+            result: elem
+        })
+        console.log(elem)
+        await ToastAndroid.show('스캔완료', ToastAndroid.SHORT);
+        this.props.navigation.navigate('Detail', { event_idx: 1 })
+    };
+
+    render() {
+        return (
+            <View>
+                <Header style={{ backgroundColor: '#311957' }}>
                     <Left style={{ flex: 1 }}>
                         <Button transparent onPress={() => this.props.navigation.goBack()}>
                             <AntDesign color='white' name='arrowleft' size={25} />
@@ -30,11 +44,23 @@ const ScanScreen = ({ navigation, route }) => {
 
                     </Right>
                 </Header>
-            <StatusBar
-                backgroundColor="#311957"
-                style={{ color: "white" }}></StatusBar>
-        </View>
-    )
+                <View>
+                    <QRCodeScanner
+                        reactivate={true}
+                        reactivateTimeout={1000}
+                        onRead={this.onSuccess}
+                        showMarker={true}
+                        checkAndroid6Permissions={true}
+                        markerStyle={{ borderColor: "#311957" }}
+                        cameraStyle={{ height: SCREEN_HEIGHT }}
+                    />
+                </View>
+                <StatusBar
+                    backgroundColor="#311957"
+                    style={{ color: "white" }}></StatusBar>
+            </View>
+        )
+    }
 }
 
 const styles = StyleSheet.create({
@@ -48,45 +74,45 @@ const styles = StyleSheet.create({
         flexDirection: 'column',
         backgroundColor: '#311957',
     },
-    title_text: {
-        fontFamily: "NotoSans-Bold",
-        color: 'white',
-    },
-    JoinContainer1: {
-        backgroundColor: '#311957',
-        flex: 2,
-        alignItems: "center"
-    },
-    JoinContainer2: {
-        backgroundColor: '#311957',
-        justifyContent: "center",
-        flex: 9,
-        alignItems: "center"
-    },
-    JoinContainer3: {
-        backgroundColor: '#311957',
-        justifyContent: "center",
-        flex: 3,
-        alignItems: "center"
-    },
-    welcome_text: {
-        marginTop: SCREEN_HEIGHT * 0.07,
-        color: 'white',
-        fontFamily: "NotoSans-Light",
-        textAlign: 'center',
-    },
-    ButtonStyle: {
-        color: 'white',
-        marginTop: SCREEN_HEIGHT * 0.16,
-        width: '35%',
-        height: SCREEN_HEIGHT * 0.05,
-        borderColor: 'white',
-        borderRadius: 100,
-        borderWidth: 1,
-        textAlign: 'center',
-        fontFamily: "NotoSans-Regular",
-        textAlignVertical: "center",
-    }
+    // title_text: {
+    //     fontFamily: "NotoSans-Bold",
+    //     color: 'white',
+    // },
+    // JoinContainer1: {
+    //     backgroundColor: '#311957',
+    //     flex: 2,
+    //     alignItems: "center"
+    // },
+    // JoinContainer2: {
+    //     backgroundColor: '#311957',
+    //     justifyContent: "center",
+    //     flex: 9,
+    //     alignItems: "center"
+    // },
+    // JoinContainer3: {
+    //     backgroundColor: '#311957',
+    //     justifyContent: "center",
+    //     flex: 3,
+    //     alignItems: "center"
+    // },
+    // welcome_text: {
+    //     marginTop: SCREEN_HEIGHT * 0.07,
+    //     color: 'white',
+    //     fontFamily: "NotoSans-Light",
+    //     textAlign: 'center',
+    // },
+    // ButtonStyle: {
+    //     color: 'white',
+    //     marginTop: SCREEN_HEIGHT * 0.16,
+    //     width: '35%',
+    //     height: SCREEN_HEIGHT * 0.05,
+    //     borderColor: 'white',
+    //     borderRadius: 100,
+    //     borderWidth: 1,
+    //     textAlign: 'center',
+    //     fontFamily: "NotoSans-Regular",
+    //     textAlignVertical: "center",
+    // }
 });
 
 export default ScanScreen;
